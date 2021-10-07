1af9b77683e7ea1dac20ac25dca3d735243b62cc
Omar Cherni <dreamdaddybanks@gmail.com>
2021-10-07 01:02:05 -0700

    6-print_line.

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* Draws a straight line in the terminal.
+* Line should end with a new line
+* If n is 0 or less, the function should only print \n
+*/
+
+void print_line(int n);
+
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
