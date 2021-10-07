59b8f1b28a823bb229926650cadf488826902186
Omar Cherni <dreamdaddybanks@gmail.com>
2021-10-07 05:10:51 -0700

    updated

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,17 +6,17 @@
 */
 
 void more_numbers(void)
-{
-	int i, ch;
 
-	for (i = 0; i < 10; i++)
-	{
-		for (ch = 0; ch < 15; ch++)
-		{
-			if (ch >= 10)
-				_putchar((ch / 10) + 48);
-			_putchar((ch % 10) + 48);
-		}
-		_putchar('\n');
-	}
+{
+int i, ch;
+for (i = 0; i < 10; i++)
+{
+for (ch = 0; ch < 15; ch++)
+{
+if (ch >= 10)
+_putchar((ch / 10) + 48);
+_putchar((ch % 10) + 48);
+}
+_putchar('\n');
+}
 }
