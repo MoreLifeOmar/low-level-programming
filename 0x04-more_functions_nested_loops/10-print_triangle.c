46093a099f3196b59fe7e582cb5ae017e36ff707
Omar Cherni <dreamdaddybanks@gmail.com>
2021-10-07 01:20:09 -0700

    10-print_triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* Print a triangle followed by a new line
+* Return: Always 0
+*/
+
+void print_triangle(int size);
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < (size - i); j++)
+			_putchar(' ');
+		for (j--; j < size; j++)
+			_putchar(35);
+		if (i < (size - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
