f8039f9292f02f485218c41ce6fa2649b843e274
Omar Cherni <dreamdaddybanks@gmail.com>
2021-10-07 04:21:49 -0700

    updated

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,8 +4,8 @@
 #include <stdio.h>
 
 int _putchar(char c)
-int _isdigit(int c);
 int _isupper(int c);
+int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
