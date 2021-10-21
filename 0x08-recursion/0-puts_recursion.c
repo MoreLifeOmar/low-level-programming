cd96639953b0663214f6917c7254630c92815bda
dreamdaddybanks <dreamdaddybanks@gmail.com>
2021-10-21 17:07:10 +0100

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,18 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- * _puts_recursion - function which print a string
- *@s: char
- *s: char
- * Return: 0
-*/
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string to print
+ *
+ * Return: void
+ */
 void _puts_recursion(char *s)
 {
-if (*s == '\0')
-{
-_putchar('\n');
-return;
-}
-else
-{
-_putchar(*s);
-_puts_recursion(s + 1);
-}
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
