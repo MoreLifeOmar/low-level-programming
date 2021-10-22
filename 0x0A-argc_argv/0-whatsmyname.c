0f4a7015e2fdd79328304a85bf947578c804766f
dreamdaddybanks <dreamdaddybanks@gmail.com>
2021-10-22 12:41:48 +0100

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print 0 argv
+ * @argc: unused
+ * @argv: array arguments call function
+ * Return: Success 0
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
