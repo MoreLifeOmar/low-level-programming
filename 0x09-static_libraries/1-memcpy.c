44eb842d426189781b442befd343a39dcd194b7e
dreamdaddybanks <dreamdaddybanks@gmail.com>
2021-10-23 14:55:46 +0100

    memcpy

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -6,7 +6,7 @@
  * @n: lenght of buffer
  * Return: new string.
  */
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
