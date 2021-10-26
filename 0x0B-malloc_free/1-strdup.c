bfb3101c289553ce702cafaecc4bd8bb64b26182
dreamdaddybanks <dreamdaddybanks@gmail.com>
2021-10-26 14:44:16 +0100

    fix strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,18 +15,17 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	for (n = 0; str[n] != '\0'; n++);
-
+	for (n = 0; str[n] != '\0'; n++)
+		;
 	strnew = (char *)malloc(n + 1 * sizeof(char));
 	if (strnew != NULL)
 	{
-	for ( i = 0; str[i] != '\0'; i++)
-	strnew[i] = str[i];
-	}
-	else
+		for (i = 0; str[i] != '\0'; i++)
+			strnew[i] = str[i];
+	} else
 	{
-	return (NULL)
+		return (NULL);
 	}
-strnew[i] = '\0';
-return (strnew);
+	strnew[i] = '\0';
+	return (strnew);
 }
