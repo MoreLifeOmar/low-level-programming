b4e73142917340877767e7746fd2859483a9e1b8
dreamdaddybanks <dreamdaddybanks@gmail.com>
2021-10-28 18:50:07 +0100

    Fixing Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,44 +1,61 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * string_nconcat - concatenate tw strings specially
- * @s1: first string
- * @s2: second string
- * @n: number of bytes of s2
- * Return: return a pointer
+ * _strlen - string len
+ * @s: string
+ *
+ * Return: int len
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
+/**
+ * string_nconcat - concatenates string s1 with nth of s2
+ * @s1: string 1
+ * @s2: string 2
+ * @n: size to take from s2
+ *
+ * Return: poiter to new string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int cont1 = 0, cont2 = 0, i;
-	char *p = NULL;
+	char *a;
+	unsigned int s1Len, s2Len;
+	unsigned int i, I;
+	char nul = '\0';
+	int tots;
 
 	if (s1 == NULL)
-	{
-		s1 = "";
-	}
+		s1 = &nul;
 	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	while (s1[cont1] != '\0')
-		cont1++;
-	while (s2[cont2] != '\0')
-		cont2++;
-	if (n >= cont2)
-		n = cont2;
-	p = malloc(cont1 + n + 1);
-	if (!p)
-		return (NULL);
-	for (i = 0; i < cont1; i++)
-	{
-		p[i] = s1[i];
-	}
-	for (i = cont1; i < (cont1 + n); i++)
-	{
-		p[i] = s2[i - cont1];
-	}
-	p[i] = '\0';
-	return (p);
+		s2 = &nul;
 
+	s1Len = _strlen(s1);
+	s2Len = _strlen(s2);
+
+	if (n >= s2Len)
+		tots = s2Len + s1Len;
+	else
+		tots = s1Len + n;
+
+	a = malloc(tots + 1);
+	if (a == NULL)
+		return (NULL);
+
+	i = 0, I = 0;
+	while (s1[i] != '\0')
+		a[I++] = s1[i++];
+	i = 0;
+	while (s2[i] != '\0' && n-- != 0)
+		a[I++] = s2[i++];
+	a[I] = '\0';
+
+	return (a);
 }
