1963fa6cb09604b4fe39c4e63a418f4ffff1a7fb
dreamdaddybanks <dreamdaddybanks@gmail.com>
2021-10-28 19:26:51 +0100

    Fixing Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,45 +1,44 @@
-#include <stdlib.h>
 #include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * *string_nconcat - concatenates n bytes of a string to another string
- * @s1: string to append to
- * @s2: string to concatenate from
- * @n: number of bytes from s2 to concatenate to s1
- *
- * Return: pointer to the resulting string
+ * *string_nconcat -  Write a function that concatenates two strings.
+ * @s1: string 1
+ * @s2: string 2
+ * @n: first bytes of s2
+ * Return: pointer to a newly allocated space in memory
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	char *s3;
+	unsigned int a = 0, b = 0, c = 0, d = 0;
 
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	if (n < len2)
-		s = malloc(sizeof(char) * (len1 + n + 1));
-	else
-		s = malloc(sizeof(char) * (len1 + len2 + 1));
-
-	if (!s)
-		return (NULL);
-
-	while (i < len1)
+	for (a = 0; s1[a] != '\0'; a++)
+		;
+	for (b = 0; s2[b] != '\0'; b++)
+		;
 	{
-		s[i] = s1[i];
-		i++;
+		if (n < b)
+			s3 = malloc(sizeof(char) * (a + n + 1));
+		else
+			s3 = malloc(sizeof(char) * (a + b + 1));
 	}
-
-	while (n < len2 && i < (len1 + n))
-		s[i++] = s2[j++];
-
-	while (n >= len2 && i < (len1 + len2))
-		s[i++] = s2[j++];
-
-	s[i] = '\0';
-
-	return (s);
+	if (s3 == NULL)
+		return (NULL);
+	for (c = 0; c < a; c++)
+		s3[c] = s1[c];
+	if (n < b)
+		for (d = 0; d < n; d++)
+		s3[c + d] = s2[d];
+	else
+		for (d = 0; d < b; d++)
+		s3[c + d] = s2[d];
+	s3[c + d]  = '\0';
+	return (s3);
+	free(s3);
 }
