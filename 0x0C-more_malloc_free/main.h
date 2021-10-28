b922b7933ab29e75bd4995df3998760e5aa98a88
dreamdaddybanks <dreamdaddybanks@gmail.com>
2021-10-28 11:05:32 +0100

    Header

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Auth: Omar Cherni
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0B-more_malloc_free directory.
- */
-
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
