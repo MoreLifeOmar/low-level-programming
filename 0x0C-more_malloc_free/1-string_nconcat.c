85c969c0c90df159de7102865b1a163b9246fdc2
dreamdaddybanks <dreamdaddybanks@gmail.com>
2021-10-28 12:18:56 +0100

    Fixing Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,13 +15,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *concat;
 	unsigned int len = n, index;
-
+	
 	if (s1 == NULL)
 		s1 = "";
-
+	
 	if (s2 == NULL)
 		s2 = "";
-
 	for (index = 0; s1[index]; index++)
 		len++;
 
