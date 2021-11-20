b0dbc21da1e0e86eb80da7fe098c082488d95c48
Omar <89669057+dreamdaddybanks@users.noreply.github.com>
2021-11-20 07:32:03 +0100

    222

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,48 +1,18 @@
 #include <stdio.h>
 #include "lists.h"
-#include <stdlib.h>
-#include <string.h>
 /**
-* add_node - A function that adds a new node at the beginning of a list_t list.
-* @head: Double pointer to the null.
-* @str: Variable type string.
-*
-* Return: The address of the new node.
-*
-*/
-list_t *add_node(list_t **head, const char *str)
+ * list_len - calculate the number of lists it contains.
+ * @h: constant list.
+ * Return: the number of nodes.
+ */
+size_t list_len(const list_t *h)
 {
-    list_t *new;
-
-    if (str == NULL)
-    {
-        return (NULL);
-    }
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        return (NULL);
-    }
-    new->str = strdup(str);
-    new->len = _strlen(str);
-    new->next = *head;
-    *head = new;
-    return (new);
+size_t l;
+l = 0;
+l++;
+if (h->next != NULL)
+{
+return (l + list_len(h->next));
 }
-/**
-* _strlen - Returns the length of a string.
-* @s: The pointer.
-*
-* Return: To the value of s.
-*/
-int _strlen(const char *s)
-{
-    int i;
-    i = 0;
-
-    while (s[i] != 0)
-    {
-        i++;
-    }
-    return (i);
+return (l);
 }
