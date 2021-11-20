10a53783050c7df127d774b9c397ce37c641536e
Omar <89669057+dreamdaddybanks@users.noreply.github.com>
2021-11-20 07:33:12 +0100

    111

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 #include "lists.h"
-
 /**
-* list_len - A function that returns the number of elements in a list.
-* @h: A constant variable of type list_t.
-*
-* Return: The number of nodes (i).
-*
-*/
+ * list_len - calculate the number of lists it contains.
+ * @h: constant list.
+ * Return: the number of nodes.
+ */
 size_t list_len(const list_t *h)
 {
-    unsigned int i;
-    const list_t *new = h;
-
-    for (i = 0; new; i++)
-    {
-        new = new->next;
-    }
-    return (i);
+size_t l;
+l = 0;
+l++;
+if (h->next != NULL)
+{
+return (l + list_len(h->next));
+}
+return (l);
 }
