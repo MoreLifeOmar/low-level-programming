67ad6226e238e7c044b3dbac9b1c4f2e4f928785
Omar Cherni <89669057+dreamdaddybanks@users.noreply.github.com>
2022-01-06 23:36:43 +0100

    Header File

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -17,6 +17,9 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
