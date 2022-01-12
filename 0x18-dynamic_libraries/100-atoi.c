6fd350c6f55d6a7e36787ca50a49444715eb4627
Omar <89669057+dreamdaddybanks@users.noreply.github.com>
2022-01-12 18:55:56 +0100

    update

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _atoi - convert string to numbers
+ *
+ * Return: n
+ */
+int atoi(void)
+{
+	/* int x; */
+	/* char arr[]; */
+
+	/* while (*s) */
+	/* { */
+	/* x++; */
+	/* if (*s >= 48 && *s <= '9') */
+	/* conv[i] = *s */
+	/* } */
+
+	return (0);
+}
