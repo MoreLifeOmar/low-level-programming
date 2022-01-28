b1621859364f9a57e87a9bd7c026290f39da51f0
Omar <89669057+dreamdaddybanks@users.noreply.github.com>
2022-01-28 23:22:42 +0100

    fixed

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,7 +1,6 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_create - function that creates a hash table.
+ * hash_table_create - a function that creates a hash table.
  * @size: hash_table size.
  * Return: hash_table_t
  */
@@ -29,4 +28,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 		new_table->array[i] = NULL;
 	new_table->size = size;
 	return (new_table);
+
 }
