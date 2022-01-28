8eb6df430b521418a7ef79e6eac4cd6c560d9b60
Omar <89669057+dreamdaddybanks@users.noreply.github.com>
2022-01-28 18:33:59 +0100

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,23 +7,25 @@
  *
  * Return: the newly created hash_table, or NULL on failure
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *h_table;
-	unsigned long int i;
+	hash_table_t *hash_table;
+	size_t i;
 
-	h_table = malloc(sizeof(hash_table_t));
-	if (h_table == NULL)
+	hash_table = malloc(sizeof(hash_table_t));
+	if (!hash_table)
 		return (NULL);
-	h_table->size = size;
-	h_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (h_table->array == NULL)
+
+	hash_table->size = size;
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!hash_table->array)
 	{
-		free(h_table);
+		free(hash_table);
 		return (NULL);
 	}
-	for (i = 0; i < size; i++)
-		h_table->array[i] = NULL;
-	return (h_table);
+	/* initialize table */
+	for (i = 0; i < size; i++) 
+			hash_table->array[i] = NULL;
+
+	return (hash_table);
 }
