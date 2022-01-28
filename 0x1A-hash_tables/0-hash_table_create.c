91148b5e9c16c3313ee6490e11733d1e3cca3466
Omar <89669057+dreamdaddybanks@users.noreply.github.com>
2022-01-28 18:36:34 +0100

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,12 +1,11 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- *
- * @size: the size of the array
- *
- * Return: the newly created hash_table, or NULL on failure
- */
+* hash_table_create - create hash table
+* @size: size of hash table
+*
+* Return: a pointer to the newly created hash table
+*/
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
@@ -23,7 +22,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(hash_table);
 		return (NULL);
 	}
-	/* initialize table */
 	for (i = 0; i < size; i++) 
 			hash_table->array[i] = NULL;
 
